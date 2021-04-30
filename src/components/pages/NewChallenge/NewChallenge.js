4c78676f8910f577b0600bab6fe6cdfdd483704c
James-W-Berry <james.berry.iv@gmail.com>
2021-04-29 21:50:20 -0400

    show invite options after successful challenge creation

--- src/components/pages/NewChallenge/NewChallenge.js
+++ src/components/pages/NewChallenge/NewChallenge.js
@@ -13,6 +13,9 @@ import {
   Input,
   InputAdornment,
   Divider,
+  Tooltip,
+  Popover,
+  Checkbox,
 } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import colors from "../../../assets/colors";
@@ -31,6 +34,7 @@ import { createNewChallenge } from "../../../api/challengeApi";
 import { useHistory } from "react-router-dom";
 import { useUserContext } from "../../../auth/UserContext";
 import { addBadge, joinChallenge } from "../../../api/userApi";
+import { Help, Lock, VisibilityOff } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -79,6 +83,7 @@ export default function NewChallenge() {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(add(new Date(), { months: 1 }));
   const [description, setDescription] = useState();
+  const [privateChallenge, setPrivateChallenge] = useState(false);
   const [participantEmail, setParticipantEmail] = useState();
   const [isParticipantEmailValid, setIsParticipantEmailValid] = useState(false);
   const [participantEmails, setParticipantEmails] = useState([]);
@@ -86,6 +91,11 @@ export default function NewChallenge() {
   const [isToastVisible, setIsToastVisible] = useState(false);
   const [toastMessage, setToastMessage] = useState("");
   const history = useHistory();
+  const [createSuccess, setCreateSuccess] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const open = Boolean(anchorEl);
+  const helpId = open ? "simple-popover" : undefined;
 
   useEffect(() => {
     setId(uuidv4());
@@ -131,6 +141,7 @@ export default function NewChallenge() {
       activity,
       startDate,
       endDate,
+      privateChallenge,
     };
 
     const isValid = validate(form);
@@ -151,7 +162,7 @@ export default function NewChallenge() {
                 title: "Challenge Creator",
               }).then((response) => {
                 console.log(response);
-                setTimeout(() => history.push(`/challenge/${form.id}`), 3000);
+                setCreateSuccess(true);
               });
             }
           });
@@ -174,153 +185,243 @@ export default function NewChallenge() {
           alignItems: "center",
         }}
       >
-        <Typography variant="h4">New Challenge</Typography>
-        <Divider />
-        <Typography variant="h5" style={{ marginTop: "40px" }}>
-          Challenge Details
-        </Typography>
-        <FormControl className={classes.formControl}>
-          <TextField
-            className={classes.field}
-            label={"What do you want to call this challenge?"}
-            placeholder={
-              "Example: Work Team Activity Challenge, Roommates Lifting Competition"
-            }
-            onChange={(event) => setTitle(event.target.value)}
-          />
-        </FormControl>
+        {createSuccess ? (
+          <div>
+            <Typography variant="h4">{title}</Typography>
 
-        <FormControl className={classes.formControl}>
-          <TextareaAutosize
-            onChange={(event) => setDescription(event.target.value)}
-            label="Description (optional)"
-            placeholder="Provide any additional information to the challenge participants (goals, prizes, inspiration, etc)"
-            rowsMin={3}
-          >
-            {description}
-          </TextareaAutosize>
-        </FormControl>
-
-        <FormControl className={classes.formControl}>
-          <InputLabel id="select-activity" className={classes.field}>
-            What kind of activity challenge do you want?
-          </InputLabel>
-          <Select
-            label="Challenge activity"
-            native
-            className={classes.field}
-            value={activity}
-            onChange={(event) => {
-              setActivity(event.target.value);
-            }}
-            labelId="select-activity"
-            id="activity-select-required"
-          >
-            <option aria-label="None" value="" />
-            {Object.entries(activityList).map((activityOption) =>
-              createActivityOption(activityOption)
-            )}
-          </Select>
-        </FormControl>
-
-        <FormControl className={classes.formControl}>
-          <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Grid container justify="space-around">
-              <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="start-date-picker"
-                label="Start date"
-                value={startDate}
-                onChange={handleStartDateChange}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-              />
-              <KeyboardDatePicker
-                disableToolbar
-                variant="inline"
-                format="MM/dd/yyyy"
-                margin="normal"
-                id="end-date-picker"
-                label="End date"
-                value={endDate}
-                onChange={handleEndDateChange}
-                KeyboardButtonProps={{
-                  "aria-label": "change date",
-                }}
-              />
-            </Grid>
-          </MuiPickersUtilsProvider>
-        </FormControl>
-
-        <Typography variant="h5" style={{ marginTop: "40px" }}>
-          Invite participants
-        </Typography>
-
-        <FormControl className={classes.formControl}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              flexDirection: "column",
-            }}
-          >
-            <Typography>
-              Share a link (anyone with this link will be able to join your
-              challenge):
+            <Typography variant="h5" style={{ marginTop: "40px" }}>
+              Invite participants now
             </Typography>
-            <Typography>
-              <a href={challengeLink}>{challengeLink}</a>
+            <FormControl className={classes.formControl}>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  flexDirection: "column",
+                }}
+              >
+                <Typography>
+                  Share a link (anyone with this link will be able to join your
+                  challenge):
+                </Typography>
+                <Typography>
+                  <a href={challengeLink}>{challengeLink}</a>
+                </Typography>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-evenly",
+                  marginTop: "10px",
+                  flexDirection: "column",
+                }}
+              >
+                <Typography>Or invite via email</Typography>
+                <Input
+                  type={"email"}
+                  value={participantEmail}
+                  placeholder="Enter participant emails"
+                  onChange={(event) =>
+                    handleParticipantEmailChange(event.target.value)
+                  }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        disabled={!isParticipantEmailValid}
+                        onClick={() =>
+                          handleAddParticipantEmail(participantEmail)
+                        }
+                      >
+                        <AddCircle />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </div>
+            </FormControl>
+
+            {participantEmails.map((email) => {
+              return <div key={email}>{email}</div>;
+            })}
+
+            <Typography variant="h5" style={{ marginTop: "40px" }}>
+              Skip Invitations for now
             </Typography>
+            <Typography style={{ marginTop: "10px" }}>
+              You can send invites or share this link later
+            </Typography>
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                style={{
+                  backgroundColor: colors.stepitup_blue,
+                  color: colors.white,
+                  marginTop: "20px",
+                  padding: "20px",
+                }}
+                onClick={() => history.push(`/challenge/${id}`)}
+              >
+                Go to challenge
+              </Button>
+            </div>
           </div>
+        ) : (
+          <div>
+            <Typography variant="h4">Create New Challenge</Typography>
+            <Divider />
+            <Typography variant="h5" style={{ marginTop: "40px" }}>
+              Challenge Details
+            </Typography>
+            <FormControl className={classes.formControl}>
+              <TextField
+                className={classes.field}
+                label={"What do you want to call this challenge?"}
+                placeholder={
+                  "Example: Work Team Activity Challenge, Roommates Lifting Competition"
+                }
+                onChange={(event) => setTitle(event.target.value)}
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <TextareaAutosize
+                onChange={(event) => setDescription(event.target.value)}
+                label="Description (optional)"
+                placeholder="Provide any additional information to the challenge participants (goals, prizes, inspiration, etc)"
+                rowsMin={3}
+              >
+                {description}
+              </TextareaAutosize>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="select-activity" className={classes.field}>
+                What kind of activity challenge do you want?
+              </InputLabel>
+              <Select
+                label="Challenge activity"
+                native
+                className={classes.field}
+                value={activity}
+                onChange={(event) => {
+                  setActivity(event.target.value);
+                }}
+                labelId="select-activity"
+                id="activity-select-required"
+              >
+                <option aria-label="None" value="" />
+                {Object.entries(activityList).map((activityOption) =>
+                  createActivityOption(activityOption)
+                )}
+              </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="start-date-picker"
+                    label="Start date"
+                    value={startDate}
+                    onChange={handleStartDateChange}
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                  />
+                  <KeyboardDatePicker
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="end-date-picker"
+                    label="End date"
+                    value={endDate}
+                    onChange={handleEndDateChange}
+                    KeyboardButtonProps={{
+                      "aria-label": "change date",
+                    }}
+                  />
+                </Grid>
+              </MuiPickersUtilsProvider>
+            </FormControl>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Typography style={{ marginTop: "20px" }}>
+                Make challenge private
+              </Typography>
+              <IconButton
+                aria-describedby={helpId}
+                onClick={(event) => setAnchorEl(event.currentTarget)}
+              >
+                <Help />
+              </IconButton>
+              <Checkbox
+                style={{ padding: "15px", color: colors.stepitup_blue }}
+                checked={privateChallenge}
+                onChange={() => setPrivateChallenge(!privateChallenge)}
+                inputProps={{ "aria-label": "private challenge" }}
+              />
 
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              marginTop: "10px",
-              flexDirection: "column",
-            }}
-          >
-            <Typography>Or invite via email</Typography>
-            <Input
-              type={"email"}
-              value={participantEmail}
-              placeholder="Enter participant emails"
-              onChange={(event) =>
-                handleParticipantEmailChange(event.target.value)
-              }
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    disabled={!isParticipantEmailValid}
-                    onClick={() => handleAddParticipantEmail(participantEmail)}
-                  >
-                    <AddCircle />
-                  </IconButton>
-                </InputAdornment>
-              }
-            />
+              <Popover
+                id={helpId}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(null)}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "center",
+                }}
+                transformOrigin={{
+                  vertical: "bottom",
+                  horizontal: "center",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    padding: "10px",
+                  }}
+                >
+                  <Lock />
+                  <Typography style={{ paddingLeft: "5px" }}>
+                    Private groups can only be joined via direct invite from the
+                    challenge creator or admin.
+                  </Typography>
+                </div>
+
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    padding: "10px",
+                  }}
+                >
+                  <VisibilityOff />
+                  <Typography style={{ paddingLeft: "5px" }}>
+                    People not in your challenge won't be able to search for or
+                    view your challenge page.
+                  </Typography>
+                </div>
+              </Popover>
+            </div>
+
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                style={{
+                  backgroundColor: colors.stepitup_blue,
+                  color: colors.white,
+                  marginTop: "20px",
+                  padding: "20px",
+                }}
+                onClick={onSubmit}
+              >
+                Create Challenge
+              </Button>
+            </div>
           </div>
-        </FormControl>
-
-        {participantEmails.map((email) => {
-          return <div key={email}>{email}</div>;
-        })}
-
-        <Button
-          style={{
-            backgroundColor: colors.stepitup_blue,
-            color: colors.white,
-            marginTop: "20px",
-          }}
-          onClick={onSubmit}
-        >
-          Create Challenge
-        </Button>
+        )}
 
         <Snackbar
           anchorOrigin={{
