743c0ad2db425f663e6655ec9ff9fe7ca5fa6816
James-W-Berry <james.berry.iv@gmail.com>
2021-05-21 00:25:13 -0400

    trying out react-google-charts

--- src/components/pages/Challenge/ChallengeTotalChart.js
+++ src/components/pages/Challenge/ChallengeTotalChart.js
@@ -1,10 +1,110 @@
-import { ResponsiveLine } from "@nivo/line";
 import { useEffect, useState } from "react";
 import colors from "../../../assets/colors";
+import { Chart } from "react-google-charts";
+
+// export default function ChallengeTotalChart(props) {
+//   const { logs, idToNameMappings } = props;
+//   const [data, setData] = useState([]);
+
+//   useEffect(() => {
+//     if (logs && idToNameMappings) {
+//       setData(convertLogsToChartData(logs));
+//       console.log(convertLogsToChartData(logs));
+//     }
+//   }, [logs, idToNameMappings]);
+
+//   const convertActivitiesToChartData = (activities) => {
+//     return activities.map((activity) => {
+//       const dateSplits = activity.date.split(" ");
+//       return {
+//         x: `${dateSplits[1]} ${dateSplits[2]}`,
+//         y: parseInt(activity.duration),
+//       };
+//     });
+//   };
+
+//   const convertLogsToChartData = (logs) => {
+//     return logs.map((log, index) => {
+//       return {
+//         id: idToNameMappings[log.participant],
+//         color: index > 1 ? "hsl(167, 70%, 50%)" : "hsl(85, 70%, 50%)",
+//         data: convertActivitiesToChartData(log.activities),
+//       };
+//     });
+//   };
+
+//   return (
+//     <ResponsiveLine
+//       data={data}
+//       margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+//       xScale={{ type: "point" }}
+//       yScale={{
+//         type: "linear",
+//         min: "auto",
+//         max: "auto",
+//         stacked: true,
+//         reverse: false,
+//       }}
+//       yFormat=" >-.2f"
+//       axisTop={null}
+//       axisRight={null}
+//       axisBottom={{
+//         orient: "bottom",
+//         tickSize: 5,
+//         tickPadding: 5,
+//         tickRotation: 0,
+//         legend: "Day",
+//         legendOffset: 36,
+//         legendPosition: "middle",
+//       }}
+//       axisLeft={{
+//         orient: "left",
+//         tickSize: 5,
+//         tickPadding: 5,
+//         tickRotation: 0,
+//         legend: "Duration",
+//         legendOffset: -40,
+//         legendPosition: "middle",
+//       }}
+//       pointSize={10}
+//       pointColor={{ theme: "background" }}
+//       pointBorderWidth={2}
+//       pointBorderColor={{ from: "serieColor" }}
+//       pointLabelYOffset={-12}
+//       useMesh={true}
+//       legends={[
+//         {
+//           anchor: "bottom-right",
+//           direction: "column",
+//           justify: false,
+//           translateX: 100,
+//           translateY: 0,
+//           itemsSpacing: 0,
+//           itemDirection: "left-to-right",
+//           itemWidth: 80,
+//           itemHeight: 20,
+//           itemOpacity: 0.75,
+//           symbolSize: 12,
+//           symbolShape: "circle",
+//           symbolBorderColor: "rgba(0, 0, 0, .5)",
+//           effects: [
+//             {
+//               on: "hover",
+//               style: {
+//                 itemBackground: "rgba(0, 0, 0, .03)",
+//                 itemOpacity: 1,
+//               },
+//             },
+//           ],
+//         },
+//       ]}
+//     />
+//   );
+// }
 
 export default function ChallengeTotalChart(props) {
   const { logs, idToNameMappings } = props;
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({ keys: [], values: [] });
 
   useEffect(() => {
     if (logs && idToNameMappings) {
@@ -24,80 +124,35 @@ export default function ChallengeTotalChart(props) {
   };
 
   const convertLogsToChartData = (logs) => {
-    return logs.map((log, index) => {
-      return {
-        id: idToNameMappings[log.participant],
-        color: index > 1 ? "hsl(167, 70%, 50%)" : "hsl(85, 70%, 50%)",
-        data: convertActivitiesToChartData(log.activities),
-      };
+    const dataKeys = ["Day", "Test Duration"];
+    const dataValues = [];
+
+    logs.forEach((log, index) => {
+      log.activities.forEach((activity) => {
+        const dateSplits = activity.date.split(" ");
+        dataValues.push([parseInt(dateSplits[2]), parseInt(activity.duration)]);
+      });
     });
+    return {
+      keys: dataKeys,
+      values: dataValues,
+    };
   };
 
   return (
-    <ResponsiveLine
-      data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-      xScale={{ type: "point" }}
-      yScale={{
-        type: "linear",
-        min: "auto",
-        max: "auto",
-        stacked: true,
-        reverse: false,
-      }}
-      yFormat=" >-.2f"
-      axisTop={null}
-      axisRight={null}
-      axisBottom={{
-        orient: "bottom",
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "Day",
-        legendOffset: 36,
-        legendPosition: "middle",
-      }}
-      axisLeft={{
-        orient: "left",
-        tickSize: 5,
-        tickPadding: 5,
-        tickRotation: 0,
-        legend: "Duration",
-        legendOffset: -40,
-        legendPosition: "middle",
-      }}
-      pointSize={10}
-      pointColor={{ theme: "background" }}
-      pointBorderWidth={2}
-      pointBorderColor={{ from: "serieColor" }}
-      pointLabelYOffset={-12}
-      useMesh={true}
-      legends={[
-        {
-          anchor: "bottom-right",
-          direction: "column",
-          justify: false,
-          translateX: 100,
-          translateY: 0,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 80,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          symbolBorderColor: "rgba(0, 0, 0, .5)",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1,
-              },
-            },
-          ],
+    <Chart
+      width={"600px"}
+      height={"400px"}
+      chartType="Line"
+      loader={<div>Loading Chart</div>}
+      data={[data.keys, ...data.values]}
+      options={{
+        chart: {
+          title: "Challenge Participant Daily Totals",
+          subtitle: "in minutes",
         },
-      ]}
+      }}
+      rootProps={{ "data-testid": "3" }}
     />
   );
 }
