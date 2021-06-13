fe0692b8ca65c041765b8ffaa4a389a648af7fc7
James-W-Berry <james.berry.iv@gmail.com>
2021-06-13 16:25:54 -0400

    update color pallette

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -2,16 +2,11 @@ const colors = {
   almostBlack: "#191919",
   almostWhite: "#eff1f0",
   white: "#ffffff",
-  gray: "#505050",
-  stepitup_teal: "#50c9c3",
-  stepitup_lightTeal: "#96deda",
-  stepitup_blueishGray: "#d7e1ec",
-  stepitup_green: "#66cc33",
-  stepitup_fadedGreen: "#66cc3380",
-  stepitup_vibrantGreen: "#93f9b9",
-  stepitup_blue: "#0076B8",
-  stepitup_fadedBlue: "#0099ff90",
-  stepitup_purple: "#3F3047",
+   stepQuestGray: "#2B3240",
+  stepQuestYellow: "#F2D06B",
+  stepQuestLightOrange: "#F2C166",
+  stepQuestPaleOrange: "#F2D6A2",
+  stepQuestOrange: "#D98236", 
 };
 
 export default colors;
